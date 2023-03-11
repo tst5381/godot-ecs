81736013245b6f5f22bbb27ec8bd827d7cbc9bc9
DESKTOP-T51DEGR\Tang <tst5381@gmail.com>
2023-03-12 01:15:17 +0800

    Refactoring - update EcsWorld data structure to efficiently iterate component values; Extending EcsSystem becomes much easier. EcsSystem is now configured via Godot editor instead.

--- src/EcsSystem.gd
+++ src/EcsSystem.gd
@@ -1,53 +1,26 @@
 class_name EcsSystem
 extends Node
 
-var _target_component : int;
-var _observe_add: bool;
-var _observe_remove: bool;
-var _observe_update: bool;
+@export var world: EcsWorld;
+@export var observe_add: bool;
+@export var observe_remove: bool;
+@export var observe_update: bool;
 
-func _init(component: int, observe_add: bool, observe_remove: bool, observe_update: bool):
-	_target_component = component;
-	_observe_add = observe_add;
-	_observe_remove = observe_remove;
-	_observe_update = observe_update;
+func _ready():
+	if world != null: world.add_system(self);
 
-func include(world : EcsWorld):
-	if _observe_add:
-		world.component_added.connect(_on_component_added);
-	if _observe_remove:
-		world.component_removed.connect(_on_component_removed);
-	if _observe_update:
-		world.component_updated.connect(_on_component_updated);
+# override to react to specific component events.
+func is_observing(_component: int) -> bool:
+	return false;
 
-func exclude(world : EcsWorld):
-	if _observe_add:
-		world.component_added.disconnect(_on_component_added);
-	if _observe_remove:
-		world.component_removed.disconnect(_on_component_removed);
-	if _observe_update:
-		world.component_updated.disconnect(_on_component_updated);
+# override to do things on event.
+func on_added(_entity: int, _component: int, _value):
+	pass;
 
-func _on_component_added(entity: int, component: int, value):
-	if component == _target_component:
-		_on_target_added(entity, component, value);
+# override to do things on event.
+func on_removed(_entity: int, _component: int, _value):
+	pass;
 
-func _on_component_removed(entity: int, component: int, value):
-	if component == _target_component:
-		_on_target_removed(entity, component, value);
-
-func _on_component_updated(entity: int, component: int, before, after):
-	if component == _target_component:
-		_on_target_updated(entity, component, before, after);
-
-func _on_target_added(entity: int, component: int, value):
-	print("component id '%s' value = '%s' is added to entity #%s" % [component, value, entity]);
-	return;
-
-func _on_target_removed(entity: int, component: int, value):
-	print("component id '%s' value = '%s' is removed from entity #%s" % [component, value, entity]);
-	return;
-
-func _on_target_updated(entity: int, component: int, before, after):
-	print("component id '%s' value is updated from '%s' to '%s' on entity #%s" % [component, before, after, entity]);
-	return;
+# override to do things on event.
+func on_updated(_entity: int, _component: int, _before, _after):
+	pass;
--- src/EcsWorld.gd
+++ src/EcsWorld.gd
@@ -2,10 +2,8 @@ class_name EcsWorld
 extends Node
 
 const ERROR_NOT_EXIST = "Entity #%s does not exist.";
-
-var _next_id: int = 0;
-var _entities: Dictionary = {};
-var _filters: Array[EcsFilter] = [];
+const ERROR_ADD = "Cannot add component id '%s' since entity #%s already has one.";
+const ERROR_UPDATE = "Cannot update component id '%s' since entity #%s doesn't have one.";
 
 signal entity_created(entity: int);
 signal entity_destroyed(entity: int, components: Dictionary);
@@ -13,78 +11,120 @@ signal component_added(entity: int, component: int, value);
 signal component_removed(entity: int, component: int, value);
 signal component_updated(entity: int, component: int, before, after);
 
+var _next_id: int = 0;
+var _component_pools: Dictionary = {};
+var _exist_list: Array[bool] = [];
+var _filters: Array[EcsFilter] = [];
+var _systems: Array[EcsSystem] = [];
+
+func add_system(system: EcsSystem):
+	if !_systems.has(system): _systems.append(system);
+
+func remove_system(system: EcsSystem):
+	if _systems.has(system): _systems.erase(system);
+
 func get_entities() -> Array[int]:
 	var array: Array[int] = [];
-	array.append_array(_entities.keys());
+	for id in len(_exist_list):
+		if _exist_list[id] == true:
+			array.append(id);
 	return array;
 
 func get_entities_to_ref() -> Array[EcsRef]:
 	var array: Array[EcsRef] = [];
-	for key in _entities.keys():
-		array.append(EcsRef.new(key, self));
+	for id in len(_exist_list):
+		if _exist_list[id] == true:
+			array.append(EcsRef.new(id, self));
 	return array;
+
+func get_component_values(component: int) -> Array:
+	var pool = _get_component_pool(component);
+	return pool.values().duplicate();
 
 func create_entity() -> int:
 	var id = _next_id;
-	_entities[id] = {}; # dictionary as component's id-value pairs 
+	_exist_list.append(true);
 	_next_id += 1;
+	assert(_next_id == _exist_list.size());
 	for filter in _filters:
 		filter._on_entity_created(id);
 	entity_created.emit(id);
 	return id;
-	
+
 func create_entity_to_ref() -> EcsRef:
 	return EcsRef.new(create_entity(), self);
 
 func destroy_entity(entity: int) -> void:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	var components: Dictionary = _entities[entity];
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	var components: Dictionary = {};
+	for component in _component_pools.keys():
+		var pool = _component_pools[component];
+		if pool.has(entity):
+			components[component] = pool[entity];
+			pool.erase(entity);
 	components.make_read_only();
-	_entities.erase(entity);
+	_exist_list[entity] = false;
 	for filter in _filters:
 		filter._on_entity_destroyed(entity);
 	entity_destroyed.emit(entity, components);
 
 func exist(entity: int) -> bool:
-	return _entities.has(entity);
+	if entity >= 0 && entity < _exist_list.size() : 
+		return _exist_list[entity];
+	else: 
+		return false;
 
 func has_component(entity: int, component: int) -> bool:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	return _entities[entity].has(component);
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	return _get_component_pool(component).has(entity);
 
 func get_component(entity: int, component: int) -> Variant:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	if _entities[entity].has(component):
-		return _entities[entity][component];
-	else:
-		return null;
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	var pool = _get_component_pool(component);
+	return pool.get(entity, null);
 
 func add_component(entity: int, component: int, value: Variant) -> void:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	assert(!_entities[entity].has(component)
-	, "Cannot add component id '%s' since entity #%s already has one." % [component, entity])
-	_entities[entity][component] = value;
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	var pool = _get_component_pool(component);
+	assert(!pool.has(entity), ERROR_ADD % [component, entity]);
+	pool[entity] = value;
 	for filter in _filters:
 		filter._on_component_added(entity, component, value);
+	for system in _systems:
+		if system.observe_add && system.is_observing(component):
+			system.on_added(entity, component, value);
 	component_added.emit(entity, component, value);
 
 func remove_component(entity: int, component: int) -> bool:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	if _entities[entity].has(component):
-		var value = _entities[entity][component];
-		_entities[entity].erase(component);
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	var pool = _get_component_pool(component);
+	if pool.has(entity):
+		var value = pool[entity];
+		pool.erase(entity);
 		for filter in _filters:
 			filter._on_component_removed(entity, component, value);
+		for system in _systems:
+			if system.observe_remove && system.is_observing(component):
+				system.on_removed(entity, component, value);
 		component_removed.emit(entity, component, value);
 		return true;
 	else:
 		return false;
-		
+
 func update_component(entity: int, component: int, value: Variant) -> void:
-	assert(_entities.has(entity), ERROR_NOT_EXIST % entity);
-	assert(_entities[entity].has(component)
-	, "Cannot update component id '%s' since entity #%s doesn't have one." % [component, entity])
-	if _entities[entity].has(component):
-		var before = _entities[entity][component];
-		_entities[entity][component] = value;
-		component_updated.emit(entity, component, before, value);
+	assert(exist(entity), ERROR_NOT_EXIST % entity);
+	var pool = _get_component_pool(component);
+	assert(pool.has(entity), ERROR_UPDATE % [component, entity]);
+	var before = pool[entity];
+	pool[entity] = value;
+	for system in _systems:
+		if system.observe_update && system.is_observing(component):
+			system.on_updated(entity, component, before, value);
+	component_updated.emit(entity, component, before, value);
+
+func _get_component_pool(component: int) -> Dictionary:
+	var pool = _component_pools.get(component, null);
+	if pool != null: return pool;
+	else:
+		_component_pools[component] = {};
+		return _component_pools[component];
